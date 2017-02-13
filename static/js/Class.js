9dedb77c670fd8b7263ecd38977f643ce1847e16
Son Pham <skp011@bucknell.edu>
2017-02-13 15:18:56 -0500

    New read data files and some new classes

--- static/js/Class.js
+++ static/js/Class.js
@@ -2,6 +2,85 @@
  * Created by AC on 2/10/17.
  */
 
+////////////////
+// MODEL CLASSES
+////////////////
+
+/**
+ * A Trainee in the program
+ * @param name
+ * @param role
+ * @param id
+ * @param num_block
+ * @constructor
+ */
+function Trainee(name, role, id, num_block) {
+    this.name = name;
+    this.role = role;
+    this.id = id;
+    this.num_block = num_block;
+
+    this.base_reqs = {};
+    this.processed_reqs = {};
+}
+
+Trainee.prototype.set_requirements = function(reqs) {
+    this.base_reqs = reqs;
+    //this.processed_reqs = JSON.parse(JSON.stringify(reqs));
+};
+
+/**
+ * A Rotation with demands
+ * @param name
+ * @param id
+ * @param vacation_allowed
+ * @param min_block_length
+ * @param type
+ * @param num_block
+ * @constructor
+ */
+function Rotation(name, id, vacation_allowed = false, min_block_length = 1.0, type = "Core", num_block = 13) {
+    this.name = name;
+    this.id = id;
+    this.vacation_allowed = vacation_allowed;
+    this.min_block_length = min_block_length;
+    this.type = type;
+    this.num_block = num_block;
+
+    this.min1 = 0;
+    this.max1 = 0;
+    this.min2 = 0;
+    this.max2 = 0;
+    this.min3 = 0;
+    this.max3 = 0;
+
+    this.processed_min1 = [];
+    this.processed_max1 = [];
+    this.processed_min2 = [];
+    this.processed_max2 = [];
+    this.processed_min3 = [];
+    this.processed_max3 = [];
+}
+
+Rotation.prototype.set_rotation_demands = function(min1, max1, min2, max2, min3, max3) {
+    this.min1 = min1;
+    this.max1 = max1;
+    this.min2 = min2;
+    this.max2 = max2;
+    this.min3 = min3;
+    this.max3 = max3;
+
+    this.processed_min1 = [min1] * num_block;
+    this.processed_max1 = [max1] * num_block;
+    this.processed_min2 = [min2] * num_block;
+    this.processed_max2 = [max2] * num_block;
+    this.processed_min3 = [min3] * num_block;
+    this.processed_max3 = [max3] * num_block;
+};
+
+//////////////////
+// GRAPHIC CLASSES
+//////////////////
 class Square {
     constructor(x, y, color, rot_name, id, renderer) {
         this.x = x;
@@ -14,7 +93,6 @@ class Square {
         this.sprite.x = this.x;
         this.sprite.y = this.y;
         this.sprite.interactive = true;
-
     }
 
     draw() {
@@ -29,6 +107,36 @@ class Square {
         this.color = color;
         this.draw();
     }
-
-
 }
+
+/**
+ * ChartBars show information about the number of people at a given rotation at anytime
+ */
+function ChartBars(x, y, height, color, renderer) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.color = color;
+    this.renderer = renderer;
+}
+
+ChartBars.prototype.draw = function() {
+
+};
+
+/**
+ * Underdone bars show information about the number of rotation needed by the trainee
+ */
+function UnderdoneBars(x1, y1, x2, y2, color, renderer) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.color = color;
+    this.renderer = renderer;
+}
+
+UnderdoneBars.prototype.draw = function() {
+
+};
+
--- static/js/Visualization.js
+++ static/js/Visualization.js
@@ -2,6 +2,122 @@
  * Created by AC on 2/9/17.
  */
 
+// Initialize some important schedule variables
+var num_block = 0;
+var num_roles = 0;
+var num_trainees = 0;
+var num_rotations = 0;
+var num_pgy1 = 0;
+var num_pgy2 = 0;
+var num_pgy3 = 0;
+var trainees = [];
+var rotations = [];
+
+function read_in_data(input_text) {
+    var str_list = input_text.split("\n");
+
+    // Read the first line: num_block and num_roles
+    var line_num = 0;
+    var data = str_list[line_num].trim().split(",");
+    num_block = parseInt(data[0], 10);
+    num_roles = parseInt(data[1], 10);
+
+    // Read the number of students in each role
+    line_num += 1;
+    data = str_list[line_num].trim().split(",");
+    num_pgy1 = parseInt(data[0]);
+    num_pgy2 = parseInt(data[1]);
+    num_pgy3 = parseInt(data[2]);
+
+    // Skip the next line
+    line_num += 1;
+
+    // Read in all PGY1s, PGY2s, PGY3s
+    var name = "";
+    var id = 0;
+    var role = "PGY1";
+    var schedule = [];
+    var new_trainee = null;
+
+    for (var i = 0; i < num_pgy1; i++) {
+        line_num += 1;
+        data = str_list[line_num].trim().split(",");
+
+        // Parse the data
+        name = data[0];
+        id = parseInt(data[1]);
+        role = data[2];
+        schedule = data[3].split(".");
+
+        // Create a new trainee based on these information
+        new_trainee = new Trainee(name, role, id, num_block);
+        trainees.push(new_trainee);
+    }
+
+    for (var i = 0; i < num_pgy2; i++) {
+        line_num += 1;
+        data = str_list[line_num].trim().split(",");
+
+        // Parse the data
+        name = data[0];
+        id = parseInt(data[1]);
+        role = data[2];
+        schedule = data[3].split(".");
+
+        // Create a new trainee based on these information
+        new_trainee = new Trainee(name, role, id, num_block);
+        trainees.push(new_trainee);
+    }
+
+    for (var i = 0; i < num_pgy3; i++) {
+        line_num += 1;
+        data = str_list[line_num].trim().split(",");
+
+        // Parse the data
+        name = data[0];
+        id = parseInt(data[1]);
+        role = data[2];
+        schedule = data[3].split(".");
+
+        // Create a new trainee based on these information
+        new_trainee = new Trainee(name, role, id, num_block);
+        trainees.push(new_trainee);
+    }
+
+    // Read in all the rotations
+    line_num += 1
+    data = str_list[line_num].trim().split(",");
+    num_rotations = parseInt(data[1], 10);
+    line_num += 1 // Skip next line
+
+    var min1 = 0, max1 = 0, min2 = 0, max2 = 0, min3 = 0, max3 = 0;
+    var new_rotation = null;
+
+    for (var i = 0; i < num_rotations; i++) {
+        line_num += 1
+        data = str_list[line_num].trim().split(",");
+
+        // Parse the data
+        id = parseInt(data[0], 10);
+        name = data[1];
+        min1 = parseInt(data[2], 10);
+        max1 = parseInt(data[3], 10);
+        min2 = parseInt(data[4], 10);
+        max2 = parseInt(data[5], 10);
+        min3 = parseInt(data[6], 10);
+        max3 = parseInt(data[7], 10);
+
+        // Create a new rotation based on these information
+        new_rotation = new Rotation(name, id, num_block = num_block);
+        new_rotation.set_rotation_demands(min1, max1, min2, max2, min3, max3);
+        rotations.push(new_rotation);
+    }
+
+    // Read in the requirements now
+}
+
+console.log(rotations)
+
 var start_x = SQUARE_TOP_LEFT[0];
 var start_y = SQUARE_TOP_LEFT[1];
 
@@ -43,9 +159,30 @@ for (var i = 0; i < NUM_TRAINEE; i ++) {
             .on('mouseover', onButtonOver);
 
         // finally we push the dude into the maggots array so it it can be easily accessed later
-        maggots.push(square);
-        app.stage.addChild(spirite);
+        //maggots.push(square);
+        //app.stage.addChild(spirite);
 
+        // square.drawRect(0, 0, SQUARE_SIZE, SQUARE_SIZE);
+        //
+        // square.endFill();
+        //
+        // var texture = app.renderer.generateTexture(square);
+        //
+        // var spirite = new PIXI.Sprite(texture);
+        // spirite.buttonMode = true;
+        //
+        // spirite.x = x1;
+        // spirite.y = y1;
+        //
+        // spirite.interactive = true;
+        //
+        // spirite
+        //     .on('mouseover', onButtonOver);
+
+        // finally we push the dude into the maggots array so it it can be easily accessed late
+        squares.addChild(newSquare.sprite);
+
+        // Merge conflict here
         rot_count += 1;
     }
     trainee_count += 1;
@@ -55,18 +192,11 @@ function onButtonOver() {
     this.isOver = true;
     msg.visible = true;
     this.visible = false;
-
 }
 
 function onButtonOut() {
     this.isOver = false;
     msg.Text = "";
-}
-
-/**
- * All animation lives here
- */
-function animate() {
 }
 
 function getRandomColor() {
@@ -76,4 +206,11 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+/**
+ * All animation lives here
+ */
+function animate() {
+
 }
