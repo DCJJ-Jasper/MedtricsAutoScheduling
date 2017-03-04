5632962e36db36843071e2ffa9826b4ff805d61e
AC Li <yl015@bucknell.edu>
2017-03-03 23:57:25 -0500

    Added block number labels. However, their x positions are a little messy.

--- static/js/Visualization.js
+++ static/js/Visualization.js
@@ -122,285 +122,6 @@ underdone_list = [];
 
 // All stuffs related to the popup info window
 
-/**
- * Call pushTrainees in the python server, which sends a bunch of text for processing
- * and visualizations.
- */
-// $.getJSON('/pushTrainees', function (data) {
-//     // Read in the data
-//     var sample_text = data['data'];
-//     read_in_data(sample_text);
-//     schedule = new Schedule(trainees, rotations, num_block);
-//
-//     // Clear out all containers
-//     squares_dict = {};
-//     underdone_bars = {};
-//
-//     // Clear out all list
-//     pgy1_squares_list = [];
-//     pgy2_squares_list = [];
-//     pgy3_squares_list = [];
-//
-//     // Clean pgy label stuffs
-//     app.stage.removeChild(pgy1_container);
-//     pgy1_container = new PIXI.Container();
-//     pgy1_container.interactive = true;
-//     pgy1_container.width = 1080;
-//     pgy1_container.height = 1920;
-//     app.stage.addChild(pgy1_container);
-//
-//     app.stage.removeChild(pgy2_container);
-//     pgy2_container = new PIXI.Container();
-//     pgy2_container.interactive = true;
-//     pgy2_container.width = 1080;
-//     pgy2_container.height = 1920;
-//     app.stage.addChild(pgy2_container);
-//
-//     app.stage.removeChild(pgy3_container);
-//     pgy3_container = new PIXI.Container();
-//     pgy3_container.interactive = true;
-//     pgy3_container.width = 1080;
-//     pgy3_container.height = 1920;
-//     app.stage.addChild(pgy3_container);
-//
-//     // Clean all squares
-//     app.stage.removeChild(rot_squares_list);
-//     rot_squares_list = [];
-//
-//     // Clean all underdone container
-//     app.stage.removeChild(underdone_list);
-//     underdone_list = [];
-//
-//     // Clean all chart list
-//     app.stage.removeChild(chart_bars);
-//     chart_bars = new PIXI.Graphics();
-//     app.stage.addChild(chart_bars);
-//
-//     var start_x = SQUARE_TOP_LEFT[0];
-//     var start_y = LABEL_ROLE_TOP_LEFT_Y + LABEL_ROLE_HEIGHT + ROLE_LABEL_TRAINEE_DIST;
-//
-//     var msg = new PIXI.Text('Rotation id');
-//     msg.visible = false;
-//     // msg.position.set(num_block * (SQUARE_SIZE + SQUARE_DISTANCE), start_y);
-//
-//     // create an array to store all the sprites
-//     var maggots = [];
-//     var trainee_count = 0;
-//
-//     num_trainees = num_pgy1 + num_pgy2 + num_pgy3;
-//
-//     // Create labels for role groups
-//     // PGY1
-//     var pgy1_label = new PIXI.Text("PGY1", {
-//         fontSize: LABEL_ROLE_SIZE
-//     });
-//     pgy1_label.position.set(LABEL_ROLE_TOP_LEFT_X, LABEL_ROLE_TOP_LEFT_Y);
-//     pgy1_top_left_y = LABEL_ROLE_TOP_LEFT_Y + LABEL_ROLE_HEIGHT + ROLE_LABEL_TRAINEE_DIST;
-//     pgy1_container.addChild(pgy1_label);
-//
-//     // PGY2
-//     var pgy2_label = new PIXI.Text("PGY2", {
-//         fontSize: LABEL_ROLE_SIZE
-//     });
-//
-//     pgy2_label.position.set(LABEL_ROLE_TOP_LEFT_X, pgy1_top_left_y + num_pgy1 * LABEL_HEIGHT + GROUP_DISTANCE);
-//     pgy2_top_left_y = pgy1_top_left_y + num_pgy1 * LABEL_HEIGHT + GROUP_DISTANCE + LABEL_ROLE_HEIGHT + ROLE_LABEL_TRAINEE_DIST;
-//     pgy2_container.addChild(pgy2_label);
-//
-//     // PGY3
-//     var pgy3_label = new PIXI.Text("PGY3", {
-//         fontSize: LABEL_ROLE_SIZE
-//     });
-//     pgy3_label.position.set(LABEL_ROLE_TOP_LEFT_X, pgy2_top_left_y + num_pgy2 * LABEL_HEIGHT + GROUP_DISTANCE);
-//     pgy3_top_left_y = pgy2_top_left_y + num_pgy2 * LABEL_HEIGHT + GROUP_DISTANCE + LABEL_ROLE_HEIGHT + ROLE_LABEL_TRAINEE_DIST;
-//     pgy3_container.addChild(pgy3_label);
-//
-//     var pgy1_count = 0;
-//     var pgy2_count = 0;
-//     var pgy3_count = 0;
-//     // Create labels for all the trainees
-//     for (var t of trainees) {
-//         var trainee_label = new PIXI.Text(t.name, {
-//             fontSize: LABEL_SIZE
-//         });
-//         var start_name_label_y = 0;
-//         switch (t.role) {
-//             case "PGY1":
-//                 start_name_label_y = pgy1_top_left_y;
-//                 pgy1_count += 1;
-//                 trainee_count = pgy1_count;
-//                 trainee_label.position.set(LABEL_TOP_LEFT_X, start_name_label_y + trainee_count * LABEL_HEIGHT);
-//                 pgy1_container.addChild(trainee_label);
-//                 break;
-//             case "PGY2":
-//                 start_name_label_y = pgy2_top_left_y;
-//                 pgy2_count += 1;
-//                 trainee_count = pgy2_count;
-//                 trainee_label.position.set(LABEL_TOP_LEFT_X, start_name_label_y + trainee_count * LABEL_HEIGHT);
-//                 pgy2_container.addChild(trainee_label);
-//                 break;
-//             case "PGY3":
-//                 start_name_label_y = pgy3_top_left_y;
-//                 pgy3_count += 1;
-//                 trainee_count = pgy3_count;
-//                 trainee_label.position.set(LABEL_TOP_LEFT_X, start_name_label_y + trainee_count * LABEL_HEIGHT);
-//                 pgy3_container.addChild(trainee_label);
-//                 break;
-//         }
-//     }
-//
-//     trainee_count = 0;
-//
-//     // Drawing the static_stuffs
-//     pgy1_count = 0;
-//     pgy2_count = 0;
-//     pgy3_count = 0;
-//
-//     // Dictionaries of the containers
-//     for (var i = 0; i < rotations.length; i++) {
-//         var new_container = new PIXI.Container();
-//         new_container.interactive = true;
-//         new_container.width = 1080;
-//         new_container.height = 1920;
-//         squares_dict[rotations[i].id] = new_container;
-//         rot_squares_list.push(new_container);
-//         app.stage.addChild(new_container);
-//     }
-//
-//     // Add containers for empty and vacation
-//     var empty_container = new PIXI.Container();
-//     empty_container.interactive = true;
-//     empty_container.width = 1080;
-//     empty_container.height = 1920;
-//     squares_dict[-1] = empty_container;
-//
-//     var vac_container = new PIXI.Container();
-//     vac_container.interactive = true;
-//     vac_container.width = 1080;
-//     vac_container.height = 1920;
-//     squares_dict[-2] = empty_container;
-//
-//     // Draw out squares
-//
-//     for (var t of trainees) {
-//         var color;
-//         var role = t.role;
-//
-//         var start_name_label_y = 0;
-//         switch (role) {
-//             case "PGY1":
-//                 start_name_label_y = pgy1_top_left_y;
-//                 pgy1_count += 1;
-//                 trainee_count = pgy1_count;
-//                 break;
-//             case "PGY2":
-//                 start_name_label_y = pgy2_top_left_y;
-//                 pgy2_count += 1;
-//                 trainee_count = pgy2_count;
-//                 break;
-//             case "PGY3":
-//                 start_name_label_y = pgy3_top_left_y;
-//                 pgy3_count += 1;
-//                 trainee_count = pgy3_count;
-//                 break;
-//         }
-//
-//         var rot_count = 0;
-//         for (var j = 0; j < num_block; j++) {
-//             var id = t.scheduled_blocks[j];
-//
-//             t.base_reqs[id] -= 1;
-//
-//             color = convert_to_color_code(ROTATIONS_COLOR[id]);
-//
-//             var x = start_x + rot_count * UNIT_RANGE;
-//             var y = start_name_label_y + trainee_count * UNIT_RANGE;
-//
-//             var newSquare = new Square(x, y, color, '', id, role, app.renderer);
-//             newSquare.draw();
-//
-//             newSquare.sprite
-//                 .on('mousedown', onSquarePressed);
-//
-//             squares_dict[id.toString()].addChild(newSquare.sprite);
-//
-//             switch (role) {
-//                 case "PGY1":
-//                     pgy1_squares_list.push(newSquare.sprite);
-//                     break;
-//                 case "PGY2":
-//                     pgy2_squares_list.push(newSquare.sprite);
-//                     break;
-//                 case "PGY3":
-//                     pgy3_squares_list.push(newSquare.sprite);
-//                     break;
-//             }
-//
-//             rot_count += 1;
-//         }
-//     }
-//
-//     // Draw out underdone bars on the right by using PIXI.Graphics
-//     pgy1_count = 0;
-//     pgy2_count = 0;
-//     pgy3_count = 0;
-//     var underdone_top_left_x = 300 + UNIT_RANGE * num_block + 40;
-//     var base_x = underdone_top_left_x;
-//
-//     for (var r of rotations) {
-//         var new_graphic = new PIXI.Graphics();
-//         underdone_bars[r.id] = new_graphic;
-//         underdone_list.push(new_graphic);
-//         app.stage.addChild(new_graphic);
-//     };
-//
-//     var base_x = underdone_top_left_x;
-//     var base_y;
-//     for (var trainee_i = 0; trainee_i < trainees.length; trainee_i++) {
-//         var t = trainees[trainee_i];
-//         var underdone_arr = t.get_underdone_array();
-//         console.log(underdone_arr)
-//
-//         switch (t.role) {
-//             case "PGY1": base_y = pgy1_top_left_y; pgy1_count += 1; trainee_count = pgy1_count; break;
-//             case "PGY2": base_y = pgy2_top_left_y; pgy2_count += 1; trainee_count = pgy2_count; break;
-//             case "PGY3": base_y = pgy3_top_left_y; pgy3_count += 1; trainee_count = pgy3_count; break;
-//         }
-//
-//         for (var j = 0; j < id_list.length; j++) {
-//             var rot_id = id_list[j]
-//             var color = convert_to_color_code(ROTATIONS_COLOR[rot_id]);
-//             var graphic = underdone_bars[rot_id];
-//
-//             console.log(trainee_count)
-//             // Calculate points
-//             if (j == 0) {
-//                 var x1 = base_x;
-//                 var y1 = base_y + UNDERDONE_UNIT_RANGE * trainee_count;
-//                 var x2 = base_x + UNDERDONE_UNIT_LENGTH * underdone_arr[j];
-//                 var y2 = base_y + UNDERDONE_UNIT_RANGE * trainee_count + UNDERDONE_SIZE;
-//             } else {
-//                 var x1 = base_x + UNDERDONE_UNIT_LENGTH * underdone_arr[j - 1];
-//                 var y1 = base_y + UNDERDONE_UNIT_RANGE * trainee_count;
-//                 var x2 = base_x + UNDERDONE_UNIT_LENGTH * underdone_arr[j];
-//                 var y2 = base_y + UNDERDONE_UNIT_RANGE * trainee_count + UNDERDONE_SIZE;
-//             }
-//
-//             // Draw the rectangle
-//             graphic.beginFill(color);
-//             graphic.moveTo(x1, y1);
-//             graphic.lineTo(x1, y2);
-//             graphic.lineTo(x2, y2);
-//             graphic.lineTo(x2, y1);
-//             graphic.lineTo(x1, y1);
-//             graphic.endFill();
-//         }
-//     }
-// });
-
-// $.getJSON('/requestToSchedule', function (data) {
-//     console.log(data)
-// });
 
 /////////////////////
 // INTERACTION HELPER
@@ -603,7 +324,6 @@ function resetBlur() {
 
 var isShown = false;
 var isScheduled = false;
-
 /**
  * When schedule button is clicked
  */
@@ -702,6 +422,22 @@ $('#schedule_btn').click(function onSchedulePressed() {
                     pgy3_label.position.set(LABEL_ROLE_TOP_LEFT_X, pgy2_top_left_y + num_pgy2 * LABEL_HEIGHT + GROUP_DISTANCE);
                     pgy3_top_left_y = pgy2_top_left_y + num_pgy2 * LABEL_HEIGHT + GROUP_DISTANCE + LABEL_ROLE_HEIGHT + ROLE_LABEL_TRAINEE_DIST;
                     pgy3_container.addChild(pgy3_label);
+
+                    // Draw block number labels
+                    var blockNumberLabelPositions = new Array(13);
+                    blockNumberLabelPositions[0] = SQUARE_TOP_LEFT[0] + SQUARE_SIZE * 4 / 2;
+                    for (var i = 1; i < (num_block + 1) / 4; i++) {
+                        if (i < 10) {
+                            blockNumberLabelPositions[i] = blockNumberLabelPositions[i - 1] + SQUARE_SIZE * 4 / 2 + SQUARE_SIZE * 4 / 2 + SQUARE_DISTANCE + (LABEL_SIZE - 1) / 2;
+                        } else {
+                            blockNumberLabelPositions[i] = blockNumberLabelPositions[i - 1] + SQUARE_SIZE * 4 / 2 + SQUARE_SIZE * 4 / 2 + SQUARE_DISTANCE + (LABEL_SIZE - 2) / 2;
+                        }
+                        var blockNumberLabel = new PIXI.Text(i, {fontSize: LABEL_SIZE});
+                        blockNumberLabel.position.set(blockNumberLabelPositions[i - 1], SQUARE_TOP_LEFT[1] - SQUARE_DISTANCE);
+                        app.stage.addChild(blockNumberLabel);
+                    }
+
+                    console.log(blockNumberLabelPositions);
 
                     var pgy1_count = 0;
                     var pgy2_count = 0;
