e5ecd97c0dd4c841cecc331ee3c915d3caa01dbd
AC Li <yl015@bucknell.edu>
2017-04-30 16:39:10 -0400

    Remove useless js libraries. Comments and arrangements of all the code.

--- static/js/Utility.js
+++ static/js/Utility.js
@@ -2,14 +2,6 @@
  * Created by Son Pham on 2/21/2017.
  */
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '0x';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
 
 function convert_to_color_code(color_tuple) {
     var r = Math.round(color_tuple[0]);
